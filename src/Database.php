162f4ba1d3583a4443bd70860e24497f8f5b3a8e
FranMunozGlez <franciscomgz11@gmail.com>
2024-11-13 08:08:35 +0100

    feature: push

---
+++ src/Database.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use PDO;
+use PDOException;
+
+class Database {
+    public $mysql;
+
+    public function __construct(){
+        try {
+            $this->mysql = $this->getConnection();
+        }   
+        catch (PDOException $e) {
+            echo "Conection with Database Failed: " .$e->getMessage();
+        }
+    }
+
+    private function getConnection() {
+        $host = "localhost";
+        $user = "root";
+        $password = "";
+        $database = "Conserjeria";
+        $charset = "utf8";
+
+        $options = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
+        $pdo = new PDO("mysql:host={$host};dbname={$database};charset={$charset}", $user, $password, $options);
+
+        return $pdo;
+    }
+}
---
+++ src/Views/Components/header.php
@@ -0,0 +1,9 @@
+
+<header>
+    <div class="container">
+        <div class="header-content">
+            <img src="./public/img/logoConcierge.png" alt="Logo" class="header-logo">
+            <h1>Welcome to the concierge management system</h1>
+        </div>
+    </div>
+</header>
